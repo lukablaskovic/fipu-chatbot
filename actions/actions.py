539096d480ad74f041e4d806a2fb827a06d7f5ae
lukablaskovic <lukablaskovic2000@gmail.com>
2023-02-25 20:13:50 +0100

    added projects for companies and their display via chatbot

--- actions/actions.py
+++ actions/actions.py
@@ -28,9 +28,70 @@ class ActionSearchCompanies(Action):
             
         return []
 
+class ActionSearchProjects(Action):
+    def name(self) -> Text:
+        return "action_show_projects"
+    
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string (company_id) is an integer."""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+    
+    def validate_company_id(
+        self,
+        id: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate company_id value."""
+        company_id = int(id)
+        if company_id in get_companies_id():
+            return {"company_id": company_id}
+        else:
+            return {"company_id": None}
+
+    def run(
+        self, 
+        dispatcher: CollectingDispatcher, 
+        tracker: Tracker, 
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        
+        company_id = tracker.get_slot("company_id")
+        validated_id = self.validate_company_id(company_id, dispatcher, tracker, domain)["company_id"]
+        if(validated_id):
+            projects = search_company_projects(company_id)
+            if projects:
+                project = "\n".join([f"\nZadatak {project['id']}:\n Kontakt email: {project['kontakt_email']}\n Opis zadatka: {project['opis_zadatka']}\n Preferirane tehnologije: {project['preferirane_tehnologije']}\n Preferencije za studenta: {project['student_preferencije']}\n Potrebno je imati: {project['potrebno_imati']}\n Trajanje: {project['trajanje_h']} sati\n Lokacija: {project['lokacija']}\n Željeno okvirno vrijeme početka: {project['okvirno_vrijeme_pocetka']}\n Angažman FIPU: {project['angazman_fipu']}\n Napomena: {project['napomena']}\n " for project in projects])
+                dispatcher.utter_message(text=f"Popis zadataka za odabranu tvrtku:\n{project}")
+            else:
+                dispatcher.utter_message(text="Za odabranu tvrtku nema definiranih zadataka. Međutim, možeš obavijestiti tvrtku da ga definira na: http://bit.ly/fipu-praksa-prijava-zadatka")
+        else:
+            dispatcher.utter_message(text="Unio si nepostojeći ID tvrtke. Molim te provjeri ponovo ili kontaktiraj administratora.")
+        return []
 
 def search_for_companies():
-  f = open("json_data/lista_poduzeca_praksa.json")
+  f = open("json_data/praksa_poduzeca_zadaci.json")
   companies = json.load(f)
   companies_list = [comp for comp in companies["lista_poduzeca"]]
   return companies_list
+
+def get_companies_id():
+    f = open("json_data/praksa_poduzeca_zadaci.json")
+    companies = json.load(f)
+    companies_id = [comp["id"] for comp in companies["lista_poduzeca"]]
+    return companies_id
+
+def search_company_projects(id):
+    f = open("json_data/praksa_poduzeca_zadaci.json")
+    companies = json.load(f)
+    for comp in companies["lista_poduzeca"]:
+        if comp["id"] == int(id):
+            zadaci = comp["zadaci"]
+    return zadaci
